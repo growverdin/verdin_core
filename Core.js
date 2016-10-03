d722cd824739691b6c72350ac8586f2cb2bff4df
Andre Roeck <roeckandre@gmail.com>
2016-10-03 04:16:28 +0000

    Starting actuation part....

--- Core.js
+++ Core.js
@@ -53,6 +53,37 @@ var executeMeasurements = function() {
 
 var executeActions = function() {
 
+	// TO BE DONE NOW..........
+
+	/*
+	//gets list of actions to be executed from the cloud
+        server.getLinkedSensors(function(linkedSensors) {
+		//if has linked sensors
+                if (linkedSensors.length > 0) {
+                        //parses list of linked sensors in array of messageObj format
+                        var messageList = messageParser.parseLinkedSensorsToMessageObjArray(linkedSensors);
+
+                        //adds each message to be sent
+                        for (message in messageList) {
+                                nodeCommunicator.addMessage(messageList[message]);
+                        }
+
+                        //start communication and adds callback to be executed on response of each node
+                        nodeCommunicator.communicate(function(messageObj) {
+                                for (var i = 0 ; i < messageObj.linkedSensorsPerDevice.length ; i++) {
+                                        //pair each linked sensor with its measurement value
+                                        var measurementObj = {
+                                                linkedSensor: messageObj.linkedSensorsPerDevice[i],
+                                                value: messageObj.responsesPerDevice[i]
+                                        };
+
+                                        //sends each of the measurements to the cloud
+                                        server.addMeasurement(measurementObj);
+                                }
+                        });
+                }
+	});
+	*/
 };
 
 var execution = function() {
